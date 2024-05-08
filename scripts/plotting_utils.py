cf888515271f2e4b0b5add43f036ee4260252250
Lorenzo Gentile <lorenzo.gentile2001@hotmail.it>
2024-05-08 16:13:19 +0200

    Move plot_waveforms in plotting_utils.py.

--- scripts/plotting_utils.py
+++ scripts/plotting_utils.py
@@ -1,6 +1,39 @@
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
+from IPython.display import Audio, display
+
+def plot_waveforms(train_ds, class_names, preprocessing_params, old_num_train_samples, num_train_samples, n):
+
+    target_sr = preprocessing_params['TARGET_SR']
+
+    if preprocessing_params['DATA_AUGMENTATION']:
+        print(f"Number of training samples before augmentation: {old_num_train_samples}")
+        print(f"Number of training samples after augmentation: {num_train_samples}")
+        # Compare the first 6 original and augmented audio clips
+        for (audio, label), (audio_augmented, _) in zip(train_ds.take(n), train_ds.skip(old_num_train_samples).take(n)):
+            print(f"Audio shape (original): {audio.shape}")
+            print(f"Audio shape (augmented): {audio_augmented.shape}")
+            display(Audio(audio, rate=target_sr))
+            display(Audio(audio_augmented, rate=target_sr))
+            plt.figure(figsize=(10, 2))
+            plt.plot(audio)
+            plt.title(f"Class: {class_names[np.argmax(label)]}")
+            plt.show()
+            plt.figure(figsize=(10, 2))
+            plt.plot(audio_augmented)
+            plt.title(f"Augmented audio")
+            plt.show()
+    else:
+        print(f"Number of training samples: {num_train_samples}")
+        # Inspect the first 6 audio clips in the training set
+        for audio, label in train_ds.take(n):
+            print(f"Audio shape: {audio.shape}")
+            display(Audio(audio, rate=target_sr))
+            plt.figure(figsize=(10, 2))
+            plt.plot(audio)
+            plt.title(f"Class: {class_names[np.argmax(label)]}")
+            plt.show()
 
 def plot_transforms(train_ds, class_names, preprocessing_params, n):
     # Determine the number of 2D and 1D transforms from the preprocessing parameters
